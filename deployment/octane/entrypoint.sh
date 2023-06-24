676cb284b9c6272f4eb926b526a590b19fbe3e4c
daanh432 <daan@daanhendriks.nl>
2023-06-24 22:49:22 +0200

    Migrate on start

--- deployment/octane/entrypoint.sh
+++ deployment/octane/entrypoint.sh
@@ -14,7 +14,8 @@ initialStuff() {
     php artisan package:discover --ansi; \
     php artisan event:cache; \
     php artisan config:cache; \
-    php artisan route:cache;
+    php artisan route:cache; \
+    php artisan migrate --force --isolated;
 }
 
 if [ "$1" != "" ]; then
